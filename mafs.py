ed97a6727cdd253e72f4e91c96afaec03b8d053c
Sebastian Pina-Otey <spinaotey@gmail.com>
2018-06-26 12:14:14 +0200

    Conditional MAF implemented

--- mafs.py
+++ mafs.py
@@ -132,7 +132,130 @@ class MaskedAutoregressiveFlow:
         Givan a dataset, calculate the random numbers used internally to generate the dataset.
         :param x: numpy array, rows are datapoints
         :param sess: tensorflow session where the graph is run
-        :return: numpy arrhttp://localhost:8888/notebooks/mafs.ipynb#ay, rows are corresponding random numbers
+        :return: numpy array, rows are corresponding random numbers
         """
 
         return sess.run(self.u,feed_dict={self.input:x})
+    
+class ConditionalMaskedAutoregressiveFlow:
+    """
+    Implements a Conditional Masked Autoregressive Flow.
+    """
+
+    def __init__(self, n_inputs, n_outputs, n_hiddens, act_fun, n_mades, batch_norm=True, momentum=0.2,
+                 output_order='sequential', mode='sequential', input=None, output=None):
+        """
+        Constructor.
+        :param n_inputs: number of (conditional) inputs
+        :param n_outputs: number of outputs
+        :param n_hiddens: list with number of hidden units for each hidden layer
+        :param act_fun: tensorflow activation function
+        :param n_mades: number of mades in the flow
+        :param batch_norm: whether to use batch normalization between mades in the flow
+        :param momentum: momentum for moving mean and variance of the batch normalization layers
+        :param output_order: order of outputs of last made
+        :param mode: strategy for assigning degrees to hidden nodes: can be 'random' or 'sequential'
+        :param input: tensorflow placeholder to serve as input; if None, a new placeholder is created
+        :param output: tensorflow placeholder to serve as output; if None, a new placeholder is created
+        """
+
+        # save input arguments
+        self.n_inputs = n_inputs
+        self.n_outputs = n_outputs
+        self.n_hiddens = n_hiddens
+        self.act_fun = act_fun
+        self.n_mades = n_mades
+        self.batch_norm = batch_norm
+        self.momentum = momentum
+        self.mode = mode
+
+        self.input = tf.placeholder(dtype=dtype,shape=[None,n_inputs],name='x') if input is None else input
+        self.y = tf.placeholder(dtype=dtype,shape=[None,n_outputs],name='y') if output is None else output
+        self.training = tf.placeholder_with_default(False,shape=(),name="training")
+        self.parms = []
+
+        self.mades = []
+        self.bns = []
+        self.u = self.y
+        self.logdet_dudy = 0.0
+
+        for i in range(n_mades):
+
+            # create a new made
+            made = mades.ConditionalGaussianMade(n_inputs, n_outputs, n_hiddens, act_fun, 
+                                                 output_order, mode, self.input, self.u)
+            self.mades.append(made)
+            self.parms += made.parms
+            output_order = output_order if output_order == 'random' else made.output_order[::-1]
+
+            # inverse autoregressive transform
+            self.u = made.u
+            self.logdet_dudy += 0.5 * tf.reduce_sum(made.logp, axis=1,keepdims=True)
+
+            # batch normalization
+            if batch_norm:
+                bn = BatchNormalizationExt(momentum=self.momentum)
+                v_tmp = tf.nn.moments(self.u,[0])[1]
+                self.u = bn.apply(self.u,training=self.training)
+                self.parms += [bn.gamma,bn.beta]
+                v_tmp = tf.cond(self.training,lambda:v_tmp,lambda:bn.moving_variance)
+                self.logdet_dudy += tf.reduce_sum(tf.log(bn.gamma)) - 0.5 * tf.reduce_sum(tf.log(v_tmp+1e-5))
+                self.bns.append(bn)
+
+        self.output_order = self.mades[0].output_order
+
+        # log likelihoods
+        self.L = tf.add(-0.5 * n_inputs * np.log(2 * np.pi) - 0.5 * tf.reduce_sum(self.u ** 2, axis=1,keepdims=True),
+                        self.logdet_dudy,name='L')
+
+        # train objective
+        self.trn_loss = -tf.reduce_mean(self.L,name='trn_loss')
+
+    def eval(self, xy, sess, log=True):
+        """
+        Evaluate log probabilities for given input-output pairs.
+        :param xy: a pair (x, y) where x rows are inputs and y rows are outputs
+        :param sess: tensorflow session where the graph is run
+        :param log: whether to return probabilities in the log domain
+        :return: log probabilities: log p(y|x)
+        """
+        
+        x, y = xy
+        lprob = sess.run(self.L,feed_dict={self.input:x,self.y:y})
+
+        return lprob if log else np.exp(lprob)
+
+    def gen(self, x, sess, n_samples=1, u=None):
+        """
+        Generate samples, by propagating random numbers through each made, after conditioning on input x.
+        :param x: input vector
+        :param sess: tensorflow session where the graph is run
+        :param n_samples: number of samples
+        :param u: random numbers to use in generating samples; if None, new random numbers are drawn
+        :return: samples
+        """
+
+        y = rng.randn(n_samples, self.n_outputs).astype(dtype) if u is None else u
+
+        if getattr(self, 'batch_norm', False):
+
+            for made, bn in zip(self.mades[::-1], self.bns[::-1]):
+                y = bn.eval_inv(sess,y)
+                y = made.gen(x, sess, n_samples, y)
+
+        else:
+
+            for made in self.mades[::-1]:
+                y = made.gen(x, sess, n_samples, y)
+
+        return y
+
+    def calc_random_numbers(self, xy):
+        """
+        Givan a dataset, calculate the random numbers used internally to generate the dataset.
+        :param xy: a pair (x, y) of numpy arrays, where x rows are inputs and y rows are outputs
+        :return: numpy array, rows are corresponding random numbers
+        """
+
+        x, y = xy
+        return sess.run(self.u,feed_dict={self.input:x,self.y:y})
